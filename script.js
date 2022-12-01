42f2d593a31010acdbd92967e76ea2655e90ed12
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2022-12-01 18:28:42 +0530

    cool fold effect

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const gallery_area = document.getElementById("gallery_container");
 users = []
 for (let key of Object.keys(jsonData)) {
     let chapter_of_html = key
-    var template_heading = document.getElementById("heading-template");
+    var template_heading = document.querySelector("[heading-template]");
     var my_heading = template_heading.content.cloneNode(true);
     my_heading.querySelector("h2").innerHTML = chapter_of_html;
     gallery_area.appendChild(my_heading);
@@ -33,41 +33,40 @@ for (let key of Object.keys(jsonData)) {
         let codeblock_of_example = chap_element["code"]
         let css_of_example = chap_element["css"]
 
-        var template_entry = document.getElementById("gallery-item-template");
+        var template_entry = document.querySelector("[gallery-item-template]");
         var my_entry = template_entry.content.cloneNode(true);
-        my_entry.querySelector(".gallery_entry").style = css_of_example;
-        // my_entry.querySelector(".gallery_entry").style = "width:20px";
-        my_entry.querySelector(".gallery_entry").src = path_of_example;
-        my_entry.querySelector(".gallery_entry").dataset.code = codeblock_of_example;
-        gallery_area.appendChild(my_entry);
-        users.push({ name: codeblock_of_example, element: my_entry })
+        let my_img = my_entry.querySelector(".gallery_entry") // why this?
+        my_img.style = css_of_example;
+        my_img.src = path_of_example;
+        my_img.dataset.code = codeblock_of_example;
+        gallery_area.appendChild(my_img);
+        users.push({ name: codeblock_of_example, img: my_img })
+
     }
 }
 
 
 console.log(users)
 
-users.forEach(user => {
-    user.name = "Hello World"
-    user.element.querySelector(".gallery_entry").style = "width:20px";
+
+
+
+const searchInput = document.querySelector("[data-search]")
+
+searchInput.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase()
+    console.log(value)
+    console.log(users)
+
+    users.forEach(user => {
+        const isVisible = user.name.toLowerCase().includes(value)
+        console.log(isVisible)
+        // user.element.classList.toggle("hide", !isVisible)
+        if (!isVisible) { 
+            user.img.style ="height:1px;";
+          }
+        if (isVisible) { 
+            user.img.style ="height: 100px;";
+        } 
+    })
 })
-
-console.log(users)
-
-// const searchInput = document.querySelector("[data-search]")
-
-// searchInput.addEventListener("input", e => {
-//     const value = e.target.value.toLowerCase()
-//     console.log(value)
-//     console.log(users)
-
-//     users.forEach(user => {
-//         const isVisible = user.name.toLowerCase().includes(value)
-//         console.log(isVisible)
-//         // user.element.classList.toggle("hide", !isVisible)
-//         if (!isVisible) { 
-            
-//             // user.element.querySelector(".gallery_entry").style ="height:20px;";
-//           }
-//     })
-// })
