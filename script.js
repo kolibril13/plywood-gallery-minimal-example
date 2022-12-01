5e9654ec250d1447f8fdb8ac8feb38eba2ebdcab
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2022-12-01 13:31:25 +0000

    Deploying to gh-pages from @ kolibril13/plywood-gallery-minimal-example@eddd71ede31bffd3f5dfc4b4f4d958e75c586f6f 🚀

--- script.js
+++ script.js
@@ -18,23 +18,50 @@ jsonData = json_content_all["plywood_content"]
 
 const gallery_area = document.getElementById("gallery_container");
 
+plywood_entries = []
 for (let key of Object.keys(jsonData)) {
     let chapter_of_html = key
-    gallery_area.insertAdjacentHTML("beforeend", `<br> <h2> ${chapter_of_html} </h2>`);
-
+    var template_heading = document.querySelector("[heading-template]");
+    var my_heading = template_heading.content.cloneNode(true);
+    my_heading.querySelector("h2").innerHTML = chapter_of_html;
+    gallery_area.appendChild(my_heading);
 
     let chapter_content = jsonData[key]
     for (const chap_element of chapter_content) {
         let path_of_example = chap_element["image_path"]
         let codeblock_of_example = chap_element["code"]
         let css_of_example = chap_element["css"]
-        gallery_area.insertAdjacentHTML("beforeend",
-            `<img 
-                src ='${path_of_example}'
-                data-code = '${codeblock_of_example}' 
-                style = '${css_of_example}'
-                onclick = 'display_code_from_gallery_cell(this);' 
-                class = 'gallery_entry' 
-                >`);
+
+        var template_entry = document.querySelector("[gallery-item-template]");
+        var my_entry = template_entry.content.cloneNode(true);
+        let my_img = my_entry.querySelector(".gallery_entry") // why this?
+        my_img.style = css_of_example;
+        my_img.ref_style = css_of_example;
+        my_img.src = path_of_example;
+        my_img.dataset.code = codeblock_of_example;
+        gallery_area.appendChild(my_img);
+        plywood_entries.push({ codesnip: codeblock_of_example, img: my_img })
+
     }
 }
+
+const searchInput = document.querySelector("[data-search]")
+
+searchInput.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase()
+
+    plywood_entries.forEach(pw_entry => {
+        const isVisible = pw_entry.codesnip.toLowerCase().includes(value)
+        console.log(isVisible)
+        if (!isVisible) {
+            pw_entry.img.style = "opacity:0.1";
+        }
+        if (isVisible) {
+            pw_entry.img.style = pw_entry.img.ref_style;
+            // pw_entry.img.style ="height:200px" // this line would have a nice folding effect
+
+        }
+
+
+    })
+})
